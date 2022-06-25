c35a555aa8713b5faa0d0eebef47e40a5f87183f
Kunalpatil22 <kunalpatil2203@gmail.com>
2022-06-25 12:18:11 +0530

    temporary changes

--- CalculateResult.js
+++ CalculateResult.js
@@ -3,42 +3,97 @@ module.exports = function CalculateResult(players = []) {
   const result = [
     {
       player_id: 1,
-      player_points: 0,
+      player_points: [],
     },
     {
       player_id: 2,
-      player_points: 0,
+      player_points: [],
     },
     {
       player_id: 3,
-      player_points: 0,
+      player_points: [],
     },
     {
       player_id: 4,
-      player_points: 0,
+      player_points: [],
     },
   ];
 
   for (let i = 0; i < players.length; i++) {
     for (let j = 0; j < players.length; j++) {
-      if (
+      if (players[i].player_id == players[j].player_id) {
+        result[i].player_points.push(null);
+      } else if (
+        players[i].player_choice == "rock" &&
+        players[j].player_choice == "rock"
+      ) {
+        result[i].player_points.push(0);
+      } else if (
+        players[i].player_choice == "rock" &&
+        players[j].player_choice == "paper"
+      ) {
+        result[i].player_points.push(0);
+      } else if (
         players[i].player_choice == "rock" &&
         players[j].player_choice == "scissor"
       ) {
-        result[i].player_points += 1;
+        result[i].player_points.push(1);
       } else if (
         players[i].player_choice == "paper" &&
         players[j].player_choice == "rock"
       ) {
-        result[i].player_points += 1;
+        result[i].player_points.push(1);
+      } else if (
+        players[i].player_choice == "paper" &&
+        players[j].player_choice == "paper"
+      ) {
+        result[i].player_points.push(0);
+      } else if (
+        players[i].player_choice == "paper" &&
+        players[j].player_choice == "scissor"
+      ) {
+        result[i].player_points.push(0);
+      } else if (
+        players[i].player_choice == "scissor" &&
+        players[j].player_choice == "rock"
+      ) {
+        result[i].player_points.push(0);
       } else if (
         players[i].player_choice == "scissor" &&
         players[j].player_choice == "paper"
       ) {
-        result[i].player_points += 1;
+        result[i].player_points.push(1);
+      } else if (
+        players[i].player_choice == "scissor" &&
+        players[j].player_choice == "scissor"
+      ) {
+        result[i].player_points.push(0);
       }
     }
   }
 
   return result;
 };
+
+// Test case:
+
+// const players = [
+//   {
+//     player_id: 1,
+//     player_choice: "paper",
+//   },
+//   {
+//     player_id: 2,
+//     player_choice: "scissor",
+//   },
+//   {
+//     player_id: 3,
+//     player_choice: "scissor",
+//   },
+//   {
+//     player_id: 4,
+//     player_choice: "rock",
+//   },
+// ];
+
+// console.log(CalculateResult(players));
